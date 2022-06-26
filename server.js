5411672753f4ae272d3566fa9f0f7021713adca0
Hoya11 <88309582+Hoya11@users.noreply.github.com>
2022-06-26 23:53:11 +0900

    [feat] Scalar and Root Types
    Scalar and Root Types

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
-// import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer, gql } from "apollo-server";
+// 같은 뜻 다른방식
+// const { ApolloServer, gql } = require("apollo-server");
 
-const { ApolloServer, gql } = require("apollo-server");
+const typeDefs = gql`
+  type User {
+    id: ID
+    username: String
+  }
+  type Tweet {
+    id: ID
+    text: String
+    author: User
+  }
+  type Query {
+    allTweets: [Tweet]
+    tweet: Tweet
+  }
+`;
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+  console.log(`Running on ${url}`);
+});
