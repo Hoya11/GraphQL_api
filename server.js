45f2a6bf5f4b531ddf52df4592d1dfa5217e98ec
Hoya11 <88309582+Hoya11@users.noreply.github.com>
2022-06-28 23:04:43 +0900

    [feat] Query Resolvers
    Query Resolvers

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ import { ApolloServer, gql } from "apollo-server";
 // 같은 뜻 다른방식
 // const { ApolloServer, gql } = require("apollo-server");
 
+const tweets = [
+  {
+    id: "1",
+    text: "first",
+  },
+  {
+    id: "2",
+    text: "second",
+  },
+];
+
 const typeDefs = gql`
   type User {
     id: ID
@@ -10,11 +21,11 @@ const typeDefs = gql`
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
   type Query {
     allTweets: [Tweet!]!
-    tweet(id: ID!): Tweet!
+    tweet(id: ID): Tweet
   }
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
@@ -22,8 +33,55 @@ const typeDefs = gql`
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
 });
+
+//----------------------
+
+// Lists and Non-Null
+
+// 아래 Character에 name에 String 타입을 사용하고 느낌표 !를 추가하여 Non-Null로 표시합니다.
+// Non-Null로 표시하게 되면 서버가 항상 이 필드에 대해 null이 아닌 값을 반환할 것으로 예상합니다. 그래서 null 값을 얻게 되면 클라이언트에게 문제가 있음을 알립니다.
+// ```
+// type Character {
+//  name: String!
+//  appearsIn: [Episode]!
+// }
+// ```
+
+//-----------------
+// Recap
+
+// - 아폴로 서버를 실행하기 위해서는 반드시 최소 1개의 Query가 필요합니다.
+// - type Query는 가장 기본적인 타입입니다.
+// - Query에 넣는 필드들은 request할 수 있는 것들이 됩니다.
+// - !를 쓰지 않으면 해당 필드는 nullable field가 됩니다. (null값을 가질 수 있는 필드)
+
+//-------------------
+// Resolvers
+
+// resolver 함수는 데이터베이스에 액세스한 다음 데이터를 반환합니다.
+// ```
+// // args는 GraphQL 쿼리의 필드에 제공된 인수입니다.
+// Query: {
+//  human(obj, args, context, info) {
+//    return context.db.loadHumanByID(args.id).then(
+//      userData => new Human(userData)
+//      )
+//   }
+// }
+// ```
